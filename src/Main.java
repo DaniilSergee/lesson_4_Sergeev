1e653b7cd17781b544f9a614a4ea9eb3cc46e16e
DaniilSergee <d0rkm4nd@gmail.com>
2023-06-13 15:36:00 +0600

    добавил дз на сообразительность

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 import java.util.Random;
 
 public class Main {
-    public static int bossHealth = 700;
+    public static int bossHealth = 1000;
     public static int bossDamage = 50;
     public static String bossDefence;
-    public static String[] heroesAttackType = {"Physical", "Magical", "Mental", "Golem", "Сleric"};
-    public static int[] heroesHealth =       {270,    260,    250,    500,    250};
-    public static int[] heroesDamage =       {10,     15,     20,     5,       0};
+    public static String[] heroesAttackType = {"Berserk", "Magical", "Mental", "Lucky", "Golem", "Thor", "Physical", "Сleric"};
+    public static int[] heroesHealth =        {270,         260,        250,      250,     500,     100,     270,     250};
+    public static int[] heroesDamage =        {10,           15,         20,       10,      5,       10,      10,     0};
     public static int roundNumber = 0;
+    public static int flag1 = 0;//крит урон
+    public static int flag2 = 0;//уклонение от босса
+    public static int flag3 = 0;//оглушение босса
+    public static int block = 20;//блок берсерка
+    public static String bigDamager;
 
     public static void main(String[] args) {
         printStatistics();
@@ -26,7 +31,7 @@ public class Main {
     }
 
     public static void healSpell() {
-        if (heroesHealth[4] > 0){
+        if (heroesHealth[7] > 0){
             int k = 0; //количество героев которым нужно лечение
             for (int i = 0; i < heroesHealth.length - 1; i++) { //медик сам себя не лечит
                 if (heroesHealth[i] > 0 && heroesHealth[i] < 100) {
@@ -40,6 +45,7 @@ public class Main {
                         m--;
                         if (m==0){
                             heroesHealth[i] += 40; //пусть N равно 40
+                            System.out.println("healSpell to " + heroesAttackType[i]);
                             break;
                         }
                     }
@@ -56,15 +62,24 @@ public class Main {
     }
 
     public static void heroesHit() {
+        flag3 = 0;
+        if (Math.random()*5 > 4 && heroesHealth[5] > 0){
+            flag3 = 1; //Босс оглушён
+        }
         for (int i = 0; i < heroesDamage.length; i++) {
             if (bossHealth > 0 && heroesHealth[i] > 0) {
-                int damage = heroesDamage[i];
+                int damage = 0;
+                if (i != 0) {
+                    damage = heroesDamage[i];
+                }else{
+                    damage = heroesDamage[i]; // Пусть берсерк вернёт 10 урона
+                }
                 if (heroesAttackType[i] == bossDefence) {
                     Random random = new Random();
                     int coeff = random.nextInt(9) + 2; // 2,3,4,5,6,7,8,9,10
                     damage = heroesDamage[i] * coeff;
-                    System.out.println("Critical damage: " + damage);
-
+                    flag1 = damage;
+                    bigDamager = heroesAttackType[i];
                 }
                 if (bossHealth - damage < 0) {
                     bossHealth = 0;
@@ -76,37 +91,88 @@ public class Main {
     }
 
     public static void bossHits() {
-        for (int i = 0; i < heroesHealth.length; i++) {
-            if (heroesHealth[3] == 0){
-                if (heroesHealth[i] > 0) {
-                    if (heroesHealth[i] - bossDamage < 0) {
-                        heroesHealth[i] = 0;
+        if (flag3 == 0 && bossHealth > 0) { // жив и оглушён ли босс
+            //код для берсерка
+            flag2 = 0;
+            if (heroesHealth[3] > 0 && (Math.random() * 6 < 2)) { //шанс уклонения 0.333
+                flag2 = 1;
+            }
+            /////////////////
+            if (heroesHealth[4] == 0) {
+                if (heroesHealth[0] > 0) {
+                    if (heroesHealth[0] - bossDamage + block <= 0) {
+                        heroesHealth[0] = 0;
                     } else {
-                        heroesHealth[i] = heroesHealth[i] - bossDamage;
+                        heroesHealth[0] = heroesHealth[0] - bossDamage + block;
+                        if (bossHealth - block <= 0) {
+                            bossHealth = 0;
+                        } else {
+                            bossHealth = bossHealth - block;
+                        }
                     }
                 }
-            } else {
-                if (heroesHealth[i] > 0) {
-                    if (heroesHealth[i] - bossDamage*4/5 < 0) {
-                        heroesHealth[i] = 0;
-                        if (heroesHealth[3] - bossDamage/5 < 0) {
-                            heroesHealth[3] = 0;
-                        }else{
-                            heroesHealth[3] = (int)(heroesHealth[3] - bossDamage/5);
+            } else{
+                if (heroesHealth[0] > 0) {
+                    if (heroesHealth[0] - bossDamage * 4 / 5 + block <= 0) {
+                        heroesHealth[0] = 0;
+                        if (heroesHealth[4] - bossDamage / 5 < 0) {
+                            heroesHealth[4] = 0;
+                        } else {
+                            heroesHealth[4] = (int) (heroesHealth[4] - bossDamage / 5);
                         }
                     } else {
 
-                        heroesHealth[i] = (int)(heroesHealth[i] - bossDamage*4/5);
-                        if (heroesHealth[3] - bossDamage/5 < 0) {
-                            heroesHealth[3] = 0;
-                        }else{
-                            heroesHealth[3] = (int)(heroesHealth[3] - bossDamage/5);
+                        heroesHealth[0] = heroesHealth[0] - (bossDamage * 4 / 5) + block;
+                        if (heroesHealth[4] - bossDamage / 5 < 0) {
+                            heroesHealth[4] = 0;
+                        } else {
+                            heroesHealth[4] = heroesHealth[4] - bossDamage / 5;
+                        }
+                        if (bossHealth - block <= 0) {
+                            bossHealth = 0;
+                        } else {
+                            bossHealth = bossHealth - block;
                         }
                     }
                 }
             }
+            /////////////
+            for (int i = 1; i < heroesHealth.length; i++) {
+                    if (i == 3 && flag2 == 1) { //шанс уклонения 0.333
+                        continue;
+                    }
+                    if (heroesHealth[4] == 0) {
+                        if (heroesHealth[i] > 0) {
+                            if (heroesHealth[i] - bossDamage < 0) {
+                                heroesHealth[i] = 0;
+                            } else {
+                                heroesHealth[i] = heroesHealth[i] - bossDamage;
+                            }
+                        }
+                    } else{
+                        if (heroesHealth[i] > 0) {
+                            if (heroesHealth[i] - bossDamage * 4 / 5 < 0) {
+                                heroesHealth[i] = 0;
+                                if (heroesHealth[4] - bossDamage / 5 < 0) {
+                                    heroesHealth[4] = 0;
+                                } else {
+                                    heroesHealth[4] = (int) (heroesHealth[4] - bossDamage / 5);
+                                }
+                            } else {
+
+                                heroesHealth[i] = (int) (heroesHealth[i] - bossDamage * 4 / 5);
+                                if (heroesHealth[4] - bossDamage / 5 < 0) {
+                                    heroesHealth[4] = 0;
+                                } else {
+                                    heroesHealth[4] = (int) (heroesHealth[4] - bossDamage / 5);
+                                }
+                            }
+                        }
+                    }
+            }
         }
     }
+
 
     public static boolean isGameOver() {
         if (bossHealth <= 0) {
@@ -138,11 +204,23 @@ public class Main {
         } else {
             defence = bossDefence;
         }*/
-        System.out.println("ROUND " + roundNumber + " --------------------");
+
         System.out.println("Boss health: " + bossHealth + " damage: " + bossDamage
                 + " defence: " + (bossDefence == null ? "No Defence" : bossDefence));
         for (int i = 0; i < heroesHealth.length; i++) {
             System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i] + " damage: " + heroesDamage[i]);
         }
+        if (flag2 == 1){
+            System.out.println("Boss - Mazila Hahaha ");
+        }
+        flag2 = 0;
+        if (bigDamager != null && bigDamager == bossDefence){
+            System.out.println("Critical damage: " + flag1);
+            System.out.println("Critical damager: " + bigDamager);
+        }
+        if (flag3 == 1 && bossHealth > 0){
+            System.out.println("Boss was stunned for the next round ");
+        }
+        System.out.println("It was ROUND^ " + roundNumber + " --------------------");
     }
 }
